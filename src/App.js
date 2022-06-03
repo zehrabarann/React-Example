2c38f1162ac17f8e5d67b6c18ae65c6bca557516
zehrabarann <brnzehraa@gmail.com>
2022-06-03 12:49:17 +0300

    React api infrastructure and lifecycle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Col, Container, Row } from 'reactstrap';
 import CategoryList from './CategoryList';
 import Navi from './Navi';
@@ -6,26 +6,55 @@ import ProductList from './ProductList';
 // import { Container ,Row ,Col } from "reactstrap"
 
 
-function App() {
-  let productInfo = {title: "ProductList", deneme:"deneme"};
-  let categoryInfo = {title: "CategoryList"};
-  return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Navi/>
-        </Row> 
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo}/>
-          </Col>
-          <Col xs="9">
-            <ProductList info ={productInfo}/>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component{
+
+  state = {currentCategory:"" ,products:[]};
+
+  componentDidMount(){
+    this.getProducts(); 
+  }
+
+
+  changeCategory = (category) =>{
+    this.setState({currentCategory:category.categoryName});
+    this.getProducts(category.id);
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if(categoryId){
+      url += "?categoryId=" + categoryId;
+
+    }
+    fetch(url)
+    .then(response => response.json())
+    .then(data => this.setState({products: data}));
+  };
+
+
+  render(){
+    let productInfo = {title: "ProductList", deneme:"deneme"};
+    let categoryInfo = {title: "CategoryList"};
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Navi/>
+          </Row> 
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory ={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo}/>
+            </Col>
+            <Col xs="9">
+              <ProductList 
+              products = {this.state.products}
+              currentCategory ={this.state.currentCategory} info ={productInfo}/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+
+  }
 }
 
-export default App;
